a486c6eaf95cc57be42677ba2e2f119b31320fd3
16111205 <16111205@facin.portoalegre.pucrsnet.br>
2019-08-27 20:44:26 -0300

    Início

---
+++ Programa.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class Programa{
+public static void main(String[] args) {
+    System.out.println("Hello World");
+    
+    JOptionPane.showMessageDialog(null, "Teste");
+    
+    for (int i=0;i<5;i++){
+        System.out.println("Hello World!");
+    }
+
+    
+
+}
+
+}
